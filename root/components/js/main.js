e8789a7e2bb18ae8c5222ec058a9243d75f35f08
WalterSiqueira <walter.siqueira2@outlook.com>
2022-03-28 17:10:47 -0300

    v0.3 complete functional

--- root/components/js/main.js
+++ root/components/js/main.js
@@ -1,154 +1,114 @@
-const delFunc = document.getElementById('btndelete');
-const dot = document.getElementById('btndot');
-const zero = document.getElementById('btnzero');
-const one = document.getElementById('btnone');
-const two = document.getElementById('btntwo');
-const three = document.getElementById('btnthree');
-const four = document.getElementById('btnfour');
-const five = document.getElementById('btnfive');
-const six = document.getElementById('btnsix');
-const seven = document.getElementById('btnseven');
-const eight = document.getElementById('btneight');
-const nine = document.getElementById('btnnine');
-const plus = document.getElementById('btnplus');
-const minus = document.getElementById('btnminus');
-const slash = document.getElementById('btnslash');
-const times = document.getElementById('btntimes');
-const resetFunc = document.getElementById('btnreset');
-const equalFunc = document.getElementById('btnequal');
-const OutputBar = document.getElementById('outputBar');
-var mathToBeDone = '';
-delFunc.addEventListener('click', function() { 
-        OutputBar.innerHTML = mathToBeDone.slice(0, mathToBeDone.length-1);
-        mathToBeDone = OutputBar.innerHTML
+const delFunc = document.getElementById('btndelete'); //variable to bring the delete button
+const dot = document.getElementById('btndot');//variable to bring the dot button
+const zero = document.getElementById('btnzero');//variable to bring the number zero button
+const one = document.getElementById('btnone');//variable to bring the number one button
+const two = document.getElementById('btntwo');//variable to bring the number two button
+const three = document.getElementById('btnthree');//variable to bring the number three button
+const four = document.getElementById('btnfour');//variable to bring the number four button
+const five = document.getElementById('btnfive');//variable to bring the number five button
+const six = document.getElementById('btnsix');//variable to bring the number six button
+const seven = document.getElementById('btnseven');//variable to bring the number seven button
+const eight = document.getElementById('btneight');//variable to bring the number eight button
+const nine = document.getElementById('btnnine');//variable to bring the number nine button
+const plus = document.getElementById('btnplus');//variable to bring the  plus operator button
+const minus = document.getElementById('btnminus');//variable to bring the subtraction operator button
+const slash = document.getElementById('btnslash');//variable to bring the division operator button
+const times = document.getElementById('btntimes');//variable to bring the multiplication operator button
+const resetFunc = document.getElementById('btnreset');//variable to bring the reset button
+const equalFunc = document.getElementById('btnequal');//variable to bring the equal button
+const OutputBar = document.getElementById('outputBar');//variable to bring the the output bar button
+var mathToBeDone = ''; //this is the control variable where the math will be stored until it is solved
+
+
+//functionality setting bellow
+delFunc.addEventListener('click', function() {  //delete button functionality
+        OutputBar.innerHTML = OutputBar.innerHTML.slice(0, OutputBar.innerHTML.length-1);//this remove the last item on the output bar
+        mathToBeDone = OutputBar.innerHTML //this sends the current value on output bar to the control variable
 })
 
-resetFunc.addEventListener('click', function() {
-        OutputBar.innerHTML = '';
-        mathToBeDone = '';
+resetFunc.addEventListener('click', function() { //reset button functionality
+        OutputBar.innerHTML = ''; //this cleans the output bar
+        mathToBeDone = ''; //this cleans the control variable
+        resolve = ''; //this cleans the answer variable
 })
-zero.addEventListener('click', function() {
-        console.log(zero.value);
-        OutputBar.innerHTML += zero.value;
-        mathToBeDone += zero.value;
-        console.log(mathToBeDone);
+zero.addEventListener('click', function() { //number zero functionality
+        OutputBar.innerHTML += zero.value;//this add to the output bar the button's value
+        mathToBeDone += zero.value;//this sends the button's value to the control variable
 });
 
-one.addEventListener('click', function() {
-        console.log(one.value);
-        OutputBar.innerHTML += one.value;
-        mathToBeDone += one.value;
-        console.log(mathToBeDone);
+one.addEventListener('click', function() { //number one functionality
+        OutputBar.innerHTML += one.value;//this add to the output bar the button's value
+        mathToBeDone += one.value;//this sends the button's value to the control variable
 });
 
-
-
-two.addEventListener('click', function() {
-        console.log(two.value);
-        OutputBar.innerHTML += two.value;
-        mathToBeDone += two.value;
-        console.log(mathToBeDone);
+two.addEventListener('click', function() { //number two functionality
+        OutputBar.innerHTML += two.value;//this add to the output bar the button's value
+        mathToBeDone += two.value;//this sends the button's value to the control variable
 });
 
-three.addEventListener('click', function() {
-        console.log(three.value);
-        OutputBar.innerHTML += three.value;
-        mathToBeDone += three.value;
-        console.log(mathToBeDone);
+three.addEventListener('click', function() { //number three functionality
+        OutputBar.innerHTML += three.value;//this add to the output bar the button's value
+        mathToBeDone += three.value;//this sends the button's value to the control variable
 });
 
-four.addEventListener('click', function() {
-        console.log(four.value);
-        OutputBar.innerHTML += four.value;
-        mathToBeDone += four.value;
-        console.log(mathToBeDone);
+four.addEventListener('click', function() { //number four functionality
+        OutputBar.innerHTML += four.value;//this add to the output bar the button's value
+        mathToBeDone += four.value;//this sends the button's value to the control variable
 });
 
-five.addEventListener('click', function() {
-        console.log(five.value);
-        OutputBar.innerHTML += five.value;
-        mathToBeDone += five.value;
-        console.log(mathToBeDone);
+five.addEventListener('click', function() { //number five functionality
+        OutputBar.innerHTML += five.value; //this add to the output bar the button's value
+        mathToBeDone += five.value;//this sends the button's value to the control variable
 });
 
-six.addEventListener('click', function() {
-        console.log(six.value);
-        OutputBar.innerHTML += six.value;
-        mathToBeDone += six.value;
-        console.log(mathToBeDone);
+six.addEventListener('click', function() { //number six functionality
+        OutputBar.innerHTML += six.value; //this add to the output bar the button's value
+        mathToBeDone += six.value;//this sends the button's value to the control variable
 });
 
 
-seven.addEventListener('click', function() {
-        console.log(seven.value);
-        OutputBar.innerHTML += seven.value;
-        mathToBeDone += seven.value;
-        console.log(mathToBeDone);
+seven.addEventListener('click', function() { //number seven functionality
+        OutputBar.innerHTML += seven.value;//this add to the output bar the button's value
+        mathToBeDone += seven.value;//this sends the button's value to the control variable
 });
 
-eight.addEventListener('click', function() {
-        console.log(eight.value);
-        OutputBar.innerHTML += eight.value;
-        mathToBeDone += eight.value;
-        console.log(mathToBeDone);
+eight.addEventListener('click', function() { //number eight functionality
+        OutputBar.innerHTML += eight.value;//this add to the output bar the button's value
+        mathToBeDone += eight.value;//this sends the button's value to the control variable
 });
 
-nine.addEventListener('click', function() {
-        console.log(nine.value);
-        OutputBar.innerHTML += nine.value;
-        mathToBeDone += nine.value;
-        console.log(mathToBeDone);
+nine.addEventListener('click', function() { //number nine functionality
+        OutputBar.innerHTML += nine.value;//this add to the output bar the button's value
+        mathToBeDone += nine.value;//this sends the button's value to the control variable
 });
 
-plus.addEventListener('click', function() {
-        console.log(plus.value);
-        OutputBar.innerHTML += plus.value;
-        mathToBeDone += plus.value;
-        console.log(mathToBeDone);
+plus.addEventListener('click', function() { //sum operator functionality
+        OutputBar.innerHTML += plus.value;//this add to the output bar the button's value
+        mathToBeDone += plus.value;//this sends the button's value to the control variable
 });
 
-minus.addEventListener('click', function() {
-        console.log(minus.value);
-        OutputBar.innerHTML += minus.value;
-        mathToBeDone += minus.value;
-        console.log(mathToBeDone);
+minus.addEventListener('click', function() { //subtraction operator functionality
+        OutputBar.innerHTML += minus.value;//this add to the output bar the button's value
+        mathToBeDone += minus.value;//this sends the button's value to the control variable
 });
 
-slash.addEventListener('click', function() {
-        console.log(slash.value);
-        OutputBar.innerHTML += slash.value;
-        mathToBeDone += slash.value;
-        console.log(mathToBeDone);
+slash.addEventListener('click', function() { //division operator functionality
+        OutputBar.innerHTML += slash.value;//this add to the output bar the button's value
+        mathToBeDone += slash.value;//this sends the button's value to the control variable
 });
 
-times.addEventListener('click', function() {
-        console.log(times.value);
-        OutputBar.innerHTML += times.value;
-        mathToBeDone += times.value;
-        console.log(mathToBeDone);
+times.addEventListener('click', function() { //multiplication operator functionality
+        OutputBar.innerHTML += times.value;//this add to the output bar the button's value
+        mathToBeDone += times.value;//this sends the button's value to the control variable
 });
 
-dot.addEventListener('click', function() {
-        console.log(dot.value);
-        OutputBar.innerHTML += dot.value;
-        mathToBeDone += dot.value;
-        console.log(mathToBeDone);
+dot.addEventListener('click', function() { //dot button functionality
+        OutputBar.innerHTML += dot.value;//this add to the output bar the button's value
+        mathToBeDone += dot.value; //this sends the button's value to the control variable
 });
-times.addEventListener('click', function() {
-        console.log(times.value);
-        OutputBar.innerHTML += times.value;
-        mathToBeDone += times.value;
-        console.log(mathToBeDone);
+equalFunc.addEventListener('click', function() { //equal button functionality
+        let resolve = eval(mathToBeDone);//this turns the control variable into the math answer
+        OutputBar.innerHTML = ''; //this cleans the output bar before showing the answer
+        OutputBar.innerHTML = resolve; //this shows on the html bar the math answer
 });
-times.addEventListener('click', function() {
-        console.log(times.value);
-        OutputBar.innerHTML += times.value;
-        mathToBeDone += times.value;
-        console.log(mathToBeDone);
-});
-times.addEventListener('click', function() {
-        console.log(times.value);
-        OutputBar.innerHTML += times.value;
-        mathToBeDone += times.value;
-        console.log(mathToBeDone);
-});
+
