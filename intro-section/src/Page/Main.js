e3ef159e6e8caaf634e87819f0f24be0b949b34e
WalterSiqueira <walter.siqueira2@outlook.com>
2022-04-22 16:11:10 -0300

    v0.1.1 mobile ready

--- intro-section/src/Page/Main.js
+++ intro-section/src/Page/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Head from  '../Components/head/head.js';
 import Content from '../Components/content/content.js';
+import Attribution from '../Components/Attribution/Attribution.js';
 import './Main.css';
 function Main() {
     return (
       <main className='main_block'>
         <Head/>
         <Content/>
+        <Attribution/>
       </main>
     )
 }
