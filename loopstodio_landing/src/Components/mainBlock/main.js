f3595c38f194b700b9f2aeba87cefbe5493286d8
Walter Siqueira <walterbarbozasiqueira@gmail.com>
2022-07-22 14:11:25 -0300

    v0.1.3 second section done

--- loopstodio_landing/src/Components/mainBlock/main.js
+++ loopstodio_landing/src/Components/mainBlock/main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Nav} from '../Nav/nav.js';
 import { Immersive } from '../ImmersiveSec/immersive.js';
+import { Interactive } from '../InteractiveSec/interactive.js';
 export function Main() {
     
     return (
         <main>
             <Nav/>
             <Immersive/>
+            <Interactive/>
         </main>
     )
 }
