ea990e5434ffc636dfdb7a34714214664d80df66
Walter Siqueira <walterbarbozasiqueira@gmail.com>
2022-07-26 14:23:34 -0300

    v0.1.5 mobile small layout ready

--- loopstodio_landing/src/Components/mainBlock/main.js
+++ loopstodio_landing/src/Components/mainBlock/main.js
@@ -3,14 +3,16 @@ import {Nav} from '../Nav/nav.js';
 import { Immersive } from '../ImmersiveSec/immersive.js';
 import { Interactive } from '../InteractiveSec/interactive.js';
 import { Creations } from '../creations/creations.js';
+import { Footer } from '../footer/footer.js';
 export function Main() {
     
     return (
-        <main>
+        <main className='mainBlock'>
             <Nav/>
             <Immersive/>
             <Interactive/>
             <Creations/>
+            <Footer/>
         </main>
     )
 }
